848d3d5ddaec1fe3bc92b502e2533461957e2707
LaVette <lavette.clifton@gmail.com>
2017-12-08 10:23:32 -0500

    made an array of font family names

---
+++ save-the-date.js
@@ -0,0 +1,13 @@
+const font_fams = [
+    anisha,
+    behindScript,
+    bolina,
+    freebooterScript,
+    hugsAndKisses,
+    mfWeddingBells,
+    mirellaScript,
+    qaskinBlack,
+    respective,
+    theHeartOfEverything,
+    youreInvited
+]
