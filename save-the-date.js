d3b35d5e2f8a812f7d4430f7536160b5e36a3bdd
LaVette <lavette.clifton@gmail.com>
2017-12-08 22:47:13 -0500

    added document.ready function in js, made font selector with populated options, corrected some font naming

--- save-the-date.js
+++ save-the-date.js
@@ -1,13 +1,33 @@
-const font_fams = [
-    anisha,
-    behindScript,
-    bolina,
-    freebooterScript,
-    hugsAndKisses,
-    mfWeddingBells,
-    mirellaScript,
-    qaskinBlack,
-    respective,
-    theHeartOfEverything,
-    youreInvited
-]
+$(document).ready(function(){
+
+    const font_fams = [
+        'anisha',
+        'behindScript',
+        'bolina',
+        'freebooterScript',
+        'hugsAndKisses',
+        'mfWeddingBells',
+        'mirellaScript',
+        'qaskinBlack',
+        'respective',
+        'theHeartOfEverything',
+        'youreInvitedHeavy'
+    ]
+
+    font_fams.forEach(font => {      
+        $('select').append('<option>'+font+'</option>')
+    })
+
+    $('#select-font').on('change', () => {
+        let fontSelected = $('#select-font').find(':selected').val()
+
+        // let optionSelected = $("option:selected", this);
+        // console.log('Option Selected: ' + optionSelected);
+
+        // let valueSelected = optionSelected.value;
+        // console.log('Value Selected: ' + valueSelected);
+        
+        $('#names, #header').css('font-family', fontSelected)
+    })
+
+});
