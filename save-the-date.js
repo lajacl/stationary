ddef6c0a665c80b3c9b1619a32d6f848788ff161
LaVette <lavette.clifton@gmail.com>
2017-12-09 16:34:51 -0500

    added an accent paper selector, creted font sizing buttons

--- save-the-date.js
+++ save-the-date.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    const font_fams = [
+    const FONTS = [
         'anisha',
         'behindScript',
         'bolina',
@@ -14,20 +14,45 @@ $(document).ready(function(){
         'youreInvitedHeavy'
     ]
 
-    font_fams.forEach(font => {      
-        $('select').append('<option>'+font+'</option>')
+    const PAPERS = [
+        'rose_silver',
+        'swirl_silver'
+    ]
+
+    FONTS.forEach(font => {      
+        $('#select-font').append('<option>' + font + '</option>')
+    })
+    
+    PAPERS.forEach(paper => {      
+        $('#select-paper').append('<option>' + paper + '</option>')
     })
 
     $('#select-font').on('change', () => {
         let fontSelected = $('#select-font').find(':selected').val()
 
-        // let optionSelected = $("option:selected", this);
-        // console.log('Option Selected: ' + optionSelected);
+        // let optionSelected = $('option:selected', this);
 
-        // let valueSelected = optionSelected.value;
+        // let valueSelected = optionSelected.val();
         // console.log('Value Selected: ' + valueSelected);
         
         $('#names, #header').css('font-family', fontSelected)
+    })    
+
+    $('#font-inc').click(() => {
+        let fontSize = parseInt($('#names, #header').css("font-size"));
+        fontSize = fontSize + 1 + 'px';
+        $('#names, #header').css({'font-size':fontSize});
+    })  
+
+    $('#font-dec').click(() => {
+        let fontSize = parseInt($('#names, #header').css("font-size"));
+        fontSize = fontSize - 1 + 'px';
+        $('#names, #header').css({'font-size':fontSize});
+    })
+
+    $('#select-paper').on('change', () => {
+        let paperSelected = $('#select-paper').find(':selected').val()
+        $('#accent-paper').css('background-image', 'url(accent-paper/' + paperSelected + '.jpg)')
     })
 
 });
