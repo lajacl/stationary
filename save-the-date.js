e32816a76976d601a861e693b87d5e090e91f913
LaVette <lavette.clifton@gmail.com>
2018-01-15 13:14:38 -0500

    added more paper selections, changed select options to radio buttons, added paper images, created options for text & cardctock papers

--- save-the-date.js
+++ save-the-date.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
         this.name = name
         this.max_size = max_size
     }
+
     
     // Create an array of Font objects
     const FONTS = [
@@ -26,48 +27,104 @@ $(document).ready(function(){
     ]
 
     // Create an array of paper file names
-    const PAPERS = [
-        'rose_silver',
-        'swirl_silver'
-    ]
+    const PAPERS = {
+        text_paper: [
+            'white_linen',
+            'cream_linen',
+            'gray_linen',
+            'white_felt'
+        ],
+        cardstock: [
+            'silver_metallic_light',
+            'silver_metallic_dark'
+        ],
+        accent_paper: [
+            'silver_rose',
+            'silver_swirl',
+            'silver_pebble'
+        ]
+    }
 
+
+    /**
+     * Loop through arrays to show design options on screen
+     */
     FONTS.forEach(font => {      
         $('#select-font').append('<option>' + font.name + '</option>')
         // console.log('Font Name: ' + font.name + ' Max Size: ' + font.max_size)
     })
     
-    PAPERS.forEach(paper => {      
-        $('#select-paper').append('<option>' + paper + '</option>')
+    PAPERS.text_paper.forEach(paper => {      
+        $('#select-text-paper').append('<input type="radio" name="selected-text-paper" value ="' + paper + '"/>' +
+        paper + ' <img src="text-paper/' + paper + '.jpg" alt ="' + paper + '" height = "50px" width = "100px"/>')
     })
+    
+    PAPERS.cardstock.forEach(paper => {      
+        $('#select-cardstock').append('<input type="radio" name="selected-cardstock" value ="' + paper + '"/>' +
+        paper + ' <img src="cardstock/' + paper + '.jpg" alt ="' + paper + '" height = "50px" width = "100px"/>')
+    })
+    
+    PAPERS.accent_paper.forEach(paper => {      
+        $('#select-accent-paper').append('<input type="radio" name="selected-accent-paper" value ="' + paper + '"/>' +
+        paper + ' <img src="accent-paper/' + paper + '.jpg" alt ="' + paper + '" height = "50px" width = "100px"/>')
+    })
+    
 
+    /**
+     * Handles user changes to form by updating view
+     */
     $('#select-font').on('change', () => {
-        let fontSelected = $('#select-font').find(':selected').val()
+        let fontSelected = getSelectedFont();
 
         // let optionSelected = $('option:selected', this);
 
         // let valueSelected = optionSelected.val();
         // console.log('Value Selected: ' + valueSelected);
         
-        $('#names, #header').css('font-family', fontSelected)
+        $('#names, #header, #select-font').css('font-family', fontSelected)
     })    
 
-    $('#font-inc').click(() => {
+    // + button clicked to increase font size
+    $('#font-increase-button').click(() => {
         let fontSize = parseInt($('#names, #header').css("font-size"))
+        if (fontSize < $())
         fontSize = fontSize + 1 + 'px'
         $('#names, #header').css({'font-size':fontSize})
         console.log('Font Size Up To: ' + fontSize)
     })  
 
-    $('#font-dec').click(() => {
+    // - button clicked to increase font size
+    $('#font-decrease-button').click(() => {
         let fontSize = parseInt($('#names, #header').css("font-size"))
         fontSize = fontSize - 1 + 'px'
         $('#names, #header').css({'font-size':fontSize})
         console.log('Font Size Down To: ' + fontSize)
     })
 
-    $('#select-paper').on('change', () => {
-        let paperSelected = $('#select-paper').find(':selected').val()
+    // radio button clicked to change text paper
+    $('input[name=selected-text-paper]').on('change', () => {
+        let paperSelected = $('input[name=selected-text-paper]:checked').val()
+        $('#content').css('background-image', 'url(text-paper/' + paperSelected + '.jpg)')
+    })
+
+    // radio button clicked to change cardstock paper
+    $('input[name=selected-cardstock]').on('change', () => {
+        let paperSelected = $('input[name=selected-cardstock]:checked').val()
+        $('#card').css('background-image', 'url(cardstock/' + paperSelected + '.jpg)')
+    })
+
+    // radio button clicked to change accent paper
+    $('input[name=selected-accent-paper]').on('change', () => {
+        let paperSelected = $('input[name=selected-accent-paper]:checked').val()
         $('#accent-paper').css('background-image', 'url(accent-paper/' + paperSelected + '.jpg)')
     })
+
+
+    /**
+     * Other Functions
+     */
+    getSelectedFont = () => {
+        return $('#select-font').find(':selected').val()
+    }
 
 });
